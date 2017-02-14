c225f0c1a6235f4ea85ddb86aaf9a0ef962e1479
radioactive3 <akshay.csen@gmail.com>
2017-02-15 02:43:10 +0530

    Added dfs, topological

--- GraphLibrary.cpp
+++ GraphLibrary.cpp
@@ -2,16 +2,24 @@
 #include<iostream>
 #include<list>
 #include <queue>
+#include<stack>
 
 using namespace std;
 class graph{
     int v ;
     list<int> *adj ;
     //void bfs_util(int a, bool visited[]);
+    bool dfs_util(int a,bool visited[]);
+    bool topological_util(int a,bool visited[],stack<int>& st);
+
+    bool cyclic_util(int a, bool visited[],bool rec[]);
 public:
     graph(int v);
     void add_edge(int v, int w);
     void bfs(int a);
+    void dfs(int a);
+    void cyclic();
+    void topological();
 
       };
 
@@ -26,6 +34,101 @@ public:
         adj[u].push_back(w);
 
     }
+
+bool graph:: cyclic_util(int a , bool visited[],bool rec[]){
+    visited[a] = true;
+    rec[a] = true;
+    list<int>:: iterator i ;
+    for(i= adj[a].begin();i!=adj[a].end();i++)
+    {
+       if(!visited[*i]&&cyclic_util(*i,visited,rec))
+       {
+           return true;
+       }
+        else if(rec[*i])
+            return true;
+    }
+    rec[a] = false;
+    return false;
+}
+void graph:: cyclic()
+{
+
+    bool* visited = new bool[v];
+    bool* rec = new bool[v];
+    for(int i = 0;i<v;i++)
+    {
+
+        visited[i]=false;
+        rec[i]= false;
+    }
+    for(int i = 0;i<v;i++){
+
+
+        if(cyclic_util(i,visited,rec)==true)
+         cout<<"has a cycle" ;
+         else
+            cout<<"no cycle found" ;
+    }
+
+}
+bool graph:: topological_util(int a, bool visited[],stack<int> &st)
+{
+    visited[a]=true;
+    list<int>:: iterator i;
+    for(i= adj[a].begin();i!=adj[a].end();i++)
+    {
+    if(!visited[*i])
+     topological_util(*i,visited,st);
+        }
+    st.push(a);
+}
+void graph:: topological()
+{
+
+    stack<int> stack ;
+    bool* visited =new bool[v];
+    for(int i=0;i<v;i++)
+        visited[i]= false;
+
+    for(int i=0;i<v;i++)
+    {
+        if (visited[i]==false)
+        topological_util(i,visited,stack);
+
+    }
+    while(!stack.empty())
+    {
+        int a = stack.top();
+        cout <<a ;
+        stack.pop();
+
+    }
+}
+bool graph:: dfs_util(int a , bool visited[])
+{
+    visited[a] =true;
+    list<int>::iterator i ;
+    cout<<a;
+    for( i =adj[a].begin();i!=adj[a].end();i++)
+    {
+        if(!visited[*i])
+            dfs_util(*i,visited);
+
+    }
+    return false;
+
+}
+void graph:: dfs(int a)
+{
+    bool* visited = new bool[v];
+    for(int i =0;i<v;i++)
+        visited[i] = 0;
+
+    visited[a]= true;
+   // for(int i=0;i<v;i++)
+        dfs_util( a, visited);
+}
 
     void graph:: bfs(int a)
     {
@@ -67,8 +170,17 @@ public:
     g.add_edge(2, 0);
     g.add_edge(2, 3);
     g.add_edge(3, 3);
+    g.topological();
+     graph g2(4);
+    g2.add_edge(0, 1);
+     g2.add_edge(0, 2);
+    g2.add_edge(1, 2);
+     g2.add_edge(2, 0);
+    g2.add_edge(2, 3);
+    g2.add_edge(3, 3);
+    //g2.cyclic();
 
-    cout << "Following is Breadth First Traversal "
-         << "(starting from vertex 2) \n";
-    g.bfs(2);
+   // cout << "Following is Breadth First Traversal "
+        // << "(starting from vertex 2) \n";
+    //g.bfs(2);
     }
